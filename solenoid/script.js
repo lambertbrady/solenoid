2b80947f171349e7923d32066cb3d7ed8ec6426d
lambertbrady <lambertbrady14@gmail.com>
2020-03-02 15:02:22 -0600

    Improved runge-kutta method builder

--- solenoid/script.js
+++ solenoid/script.js
@@ -11,6 +11,7 @@ const curry = fn => {
       }
    };
 }
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 
 function* rangeGen(start, stop, step = 1, cycles = 1, inclusive = true) {
 // function range(...args) {
@@ -153,6 +154,152 @@ function heunMethodVec(yInitial, fn, h, tInitial, tFinal) {
    return Array.from(heunMethodVecGen(...arguments));
 }
 
+function dotArray(arrA, arrB) {
+   if (arrA.length !== arrB.length) {
+      throw new Error('arrays must have equal length');
+   }
+   return arrA.reduce((sum, a, i) => {
+      return sum + a * arrB[i];
+   }, 0);
+}
+
+function rungeKuttaNext(y, t, h, weights, kArr) {
+   if (weights.length !== kArr.length) {
+      throw new Error('arrays must have equal length');
+   }
+   const sum = weights.reduce((sum, weight, i) => {
+      return sum + weight * kArr[i](y,t);
+   }, 0);
+   return y + h * sum;
+}
+
+function rungeKutta(numStages, nodes, rkMatrix, weights) {
+   // Arguments in terms of standard Runge-Kutta Method notation:
+   // numStages:
+   // s
+   // * Integer greater than or equal to 1
+   // Butcher Tableau values
+   // nodes:
+   // [c_2, c_3, ..., c_(s-1), c_s]
+   // * Array with length equal to (s-1)
+   // * Array entries are numbers in range [0,1] 
+   // ** NOTE: c_1 is always 0, so it is omitted - the nodes array for a first order Euler Method will be empty []
+   // rkMatrix (Runge-Kutta Matrix):
+   // [a_ij] = [[a_21], [a_31, 32], ..., [a_s1, a_s2, ..., a_s(s-1)]]
+   // * Array with length equal to (s-1)
+   // * Array entries (a_ij) are arrays with length equal to j = rkMatrix[index+1]
+   // * a_ij entries are numbers in range [0,1]
+   // * Sum of a_ij entries must equal the corresponding node value, a_i1 + a_i3 + ... + a_i(s-1) = c_i  <==>  sum(rkMatrix[index]) = nodes[index]
+   // ** NOTE: [a_11] is always omitted - the rKMatrix array for a first order Euler Method will be empty []
+   // weights:
+   // [b_1, b_2, ..., b_(s-1), b_s]
+   // * Array with length equal to (s)
+   // * Array entries are numbers in range [0,1] 
+   // * Sum of array entries must equal 1 (approximately, to allow for fractional rounding errors)
+
+   // numStages
+   if (numStages < 1 || !Number.isInteger(numStages)) {
+      throw new Error('numStages must be an integer greater than or equal to 1')
+   }
+   // nodes
+   if (!(nodes instanceof Array) || nodes.length !== numStages - 1) {
+      throw new Error('nodes must be an array with length equal to (numStages - 1)')
+   }
+   if (nodes.some(node => node < 0 || node > 1)) {
+      throw new Error('each node must be a number between 0 (inclusive) and 1 (inclusive)')
+   }
+   // rkMatrix
+   if (!(rkMatrix instanceof Array) || rkMatrix.length !== numStages - 1) {
+      throw new Error('rkMatrix must be an array with length equal to (numStages - 1)')
+   }
+   rkMatrix.forEach((rkEntry, entryIndex) => {
+      if (!(rkEntry instanceof Array)) {
+         throw new Error('each entry in rkMatrix must be an instanceof Array');
+      }
+      if (!(rkEntry instanceof Array) || rkEntry.length !== entryIndex + 1) {
+         throw new Error(`each entry in rkMatrix must have a length equal to (entryIndex + 1). Entry provided at index ${entryIndex} has length equal to ${rkEntry.length}, but should be ${entryIndex + 1}`);
+      }
+      const entrySum = rkEntry.reduce((sum, entryVal) => sum += entryVal, 0);
+      if (entrySum !== nodes[entryIndex]) {
+         throw new Error(`sum of values in each rkMatrix entry must be equal to the entry's corresponding node value. Sum of entry provided at index ${entryIndex} is equal to ${entrySum}, but should be ${nodes[entryIndex]}`);
+      }
+   })
+   // weights
+   if (!(weights instanceof Array) || weights.length !== numStages) {
+      throw new Error('weights must be an array with length equal to numStages')
+   }
+   if (weights.some(weight => weight < 0 || weight > 1)) {
+      throw new Error('each weight must be a number between 0 (inclusive) and 1 (inclusive)')
+   }
+   // TODO: handle fractional rounding errors
+   const weightSum = weights.reduce((sum, weight) => sum + weight, 0);
+   if (weightSum !== 1) {
+      console.warn(`sum of weights is not equal to 1. Current sum is ${weightSum}`);
+      // throw new Error('sum of all weights must equal 1');
+   }
+
+   console.log('_____');
+   return function*(yInitial, dy_dt, step = 1, tInitial = 0, tFinal = 4) {
+      // y0, f(y,t), h, t0, tn
+      let y = yInitial;
+      yield y;
+      for (let t = tInitial; t < tFinal; t += step) {
+         let slopes = [];
+         const bkSum = weights.reduce((sum, weight, stageIndex) => {
+            let y_k = y;
+            let t_k = t;
+            if (stageIndex !== 0) {
+               const akSum = rkMatrix[stageIndex-1].reduce((sum, a, j) => sum + a * slopes[j], 0);
+               y_k += step * akSum;
+               t_k += step * nodes[stageIndex-1];
+            }
+            const k = dy_dt(y_k, t_k);
+            slopes.push(k);
+            return sum + weight * slopes[stageIndex];
+         }, 0);
+         // k_1 = dy_dt(t_n, y_n)
+         // k_2 = f(k_1) = dy_dt(t_n + h * c_2, y_n + h * (a_21 * k_1))
+         // k_s = f(k_(s-1)) = dy_dt(t_n + h * c_s, y_n + h * (a_s1*k_1 + a_s2*k_2 + ... + a_s(s-1)*k_(s-1)))
+         // y_(n+1) = y_n + h * (b_1*k_1 + b_2*k_2 + ... + b_s*k_s);
+         y = y + step * bkSum;
+         yield y;
+      }
+   }
+}
+
+// Euler Method: only consistent method with 1 stage 
+// const em = rungeKutta(1, [], [], [1]);
+const y0 = 1;
+const h = 1;
+const tRange = [1,4]
+const rkFunc = (y,t) => y;
+const em = rungeKutta(1, [], [], [1])(y0, rkFunc, h, ...tRange);
+console.log(em.next());
+console.log(em.next());
+console.log(em.next());
+console.log(em.next());
+console.log(em.next());
+// // Midpoint Method
+// const mm = rungeKutta(2, [.5], [[.5]], [0, 1]);
+const mm = rungeKutta(2, [.5], [[.5]], [0, 1])(y0, rkFunc, h, ...tRange);
+console.log(mm.next());
+console.log(mm.next());
+console.log(mm.next());
+console.log(mm.next());
+console.log(mm.next());
+// // Heun Method
+// rungeKutta(2, [1], [[1]], [.5, .5]);
+// // Ralston Method
+// rungeKutta(2, [2/3], [[2/3]], [.25, .75]);
+// // RK4 Method
+const rk4 = rungeKutta(4, [.5, .5, 1], [[.5], [0, .5], [0, 0, 1]], [.167, .333, .333, .167])(y0, rkFunc, h, ...tRange);
+console.log(rk4.next());
+console.log(rk4.next());
+console.log(rk4.next());
+console.log(rk4.next());
+console.log(rk4.next());
+// rungeKutta(4, [.5, .5, 1], [[.5], [0, .5], [0, 0, 1]], [.167, .333, .333, .167]);
+
 function* RK4MethodGen(yInitial, fn, h = 1, tInitial = 0, tFinal = 4) {
    // fn = y'(y,t) = slope of curve
    let y = yInitial;
@@ -166,7 +313,8 @@ function* RK4MethodGen(yInitial, fn, h = 1, tInitial = 0, tFinal = 4) {
    const b3 = 1/3;
    const b4 = 1/6;
    for (let t = tInitial; t < tFinal; t += h) {
-      y = y + h * (b1 * k1(y,t) + b2 * k2(y,t) + b3 * k3(y,t) + b4 * k4(y,t));
+      y = rungeKuttaNext(y, t, h, [b1,b2,b3,b4], [k1,k2,k3,k4]);
+      // y = y + h * (b1 * k1(y,t) + b2 * k2(y,t) + b3 * k3(y,t) + b4 * k4(y,t));
       yield y;
    }
 }
@@ -179,15 +327,28 @@ function* RK4MethodVecGen(yInitial, fn, h = 1, tInitial = 0, tFinal = 4) {
    // fn = y'(y,t) = slope of curve
    let y = yInitial;
    yield y;
-   const k1 = (y, t) => fn(y, t);
-   const k2 = (y, t) => fn(p5.Vector.add(y, p5.Vector.div(k1(y,t), 2)), t + h/2);
-   const k3 = (y, t) => fn(p5.Vector.add(y, p5.Vector.div(k2(y,t), 2)), t + h/2);
-   const k4 = (y, t) => fn(p5.Vector.add(y, k3(y,t)), t + h);
+   const k1 = (y, t) => {
+      // console.log('1')
+      return fn(y, t);
+   }
+   const k2 = (y, t) => {
+      // console.log('2')
+      return fn(p5.Vector.add(y, p5.Vector.div(k1(y,t), 2)), t + h/2);
+   }
+   const k3 = (y, t) => {
+      // console.log('3')
+      return fn(p5.Vector.add(y, p5.Vector.div(k2(y,t), 2)), t + h/2);
+   }
+   const k4 = (y, t) => {
+      // console.log('4')
+      return fn(p5.Vector.add(y, k3(y,t)), t + h);
+   }
    const b1 = 1/6;
    const b2 = 1/3;
    const b3 = 1/3;
    const b4 = 1/6;
    for (let t = tInitial; t < tFinal; t += h) {
+      // console.log('---')
       const bk1 = p5.Vector.mult(k1(y,t), b1);
       const bk2 = p5.Vector.mult(k2(y,t), b2);
       const bk3 = p5.Vector.mult(k3(y,t), b3);
@@ -561,7 +722,7 @@ function setup() {
    // +x: right, +y: downw, +z: towards
    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
 
-   sol = new Solenoid(400, 50, 50, 15);
+   sol = new Solenoid(400, 50, 30, 15);
    const mult = 1.5;
    const w = mult * sol.R;
    const Lz = mult * sol.L;
@@ -616,8 +777,8 @@ function setup() {
       return Bhat;
    };
    // fieldLines = vArr.map(v => eulerMethodVec(v, calcBSum(sol), 5, 0, 1000).filter((_, index) => index % 10 === 0));
-   fieldLinesH = vArr.map(v => heunMethodVec(v, calcBSum(sol), 5, 0, 1000).filter((_, index) => index % 10 === 0));
-   // fieldLinesRK4 = vArr.map(v => RK4MethodVec(v, calcBSum(sol), 5, 0, 1000).filter((_, index) => index % 10 === 0));
+   // fieldLinesH = vArr.map(v => heunMethodVec(v, calcBSum(sol), 5, 0, 1000).filter((_, index) => index % 10 === 0));
+   fieldLinesRK4 = vArr.map(v => RK4MethodVec(v, calcBSum(sol), 5, 0, 1000).filter((_, index) => index % 10 === 0));
 
    // sphHelix = new SphericalHelix(200, 15, 25);
    // const h = sphHelix.R;
@@ -629,7 +790,7 @@ function setup() {
    //    })
    // }
    
-   // noLoop();
+   noLoop();
 }
 
 function draw() {
@@ -683,22 +844,10 @@ function draw() {
    //    endShape()
    //    pop();
    // })
-   fieldLinesH.forEach(fieldLine => {
-      push();
-      noFill();
-      stroke('blue');
-      beginShape()
-      for (const v of fieldLine) {
-         vertex(v.x, v.y, v.z);
-      }
-      endShape();
-      pop();
-   })
-   // fieldLinesRK4.forEach(fieldLine => {
+   // fieldLinesH.forEach(fieldLine => {
    //    push();
-   //    // normalMaterial();
    //    noFill();
-   //    stroke('cyan');
+   //    stroke('blue');
    //    beginShape()
    //    for (const v of fieldLine) {
    //       vertex(v.x, v.y, v.z);
@@ -706,6 +855,18 @@ function draw() {
    //    endShape();
    //    pop();
    // })
+   fieldLinesRK4.forEach(fieldLine => {
+      push();
+      // normalMaterial();
+      noFill();
+      stroke('black');
+      beginShape()
+      for (const v of fieldLine) {
+         vertex(v.x, v.y, v.z);
+      }
+      endShape();
+      pop();
+   })
    // testPointsSol.forEach(p => {
    //    push();
    //    translate(p.x, p.y, p.z);
